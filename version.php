bb57d970015cdd3b34b0786535e29c0e91d3c55a
Conn Warwicker <conn@cmrwarwicker.com>
2019-10-31 18:41:52 +0000

    Updated version

--- version.php
+++ version.php
@@ -23,7 +23,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2019102902;
+$plugin->version = 2019103100;
 $plugin->requires = 2016052300;
 $plugin->component = 'block_quick_course';
 $plugin->maturity   = MATURITY_STABLE;
